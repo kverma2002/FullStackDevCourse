b0c58027e6d3236aff71efc4e9783dba9942595d
Krit Verma <kverma@usc.edu>
2024-06-05 21:15:52 -0400

    1.3

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const Header = (props) => {
 
 const Part = (props) => {
   return (
-    <p>{props.info[0]} {props.info[1]}</p>
+    <p>{props.info.name} {props.info.exercises}</p>
   )
 }
 
@@ -33,15 +33,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = ['Fundamentals of React', 10]
-  const part2 = ['Using props to pass data', 7]
-  const part3 = ['State of a component', 14]
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
       <Content part1={part1} part2={part2} part3={part3}/>
-      <Total total={part1[1] + part2[1] + part3[1]}/>
+      <Total total={part1.exercises + part2.exercises + part3.exercises}/>
     </div>
   )
 }
